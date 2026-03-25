36b75d47398c0289609196a1a6ea1302a900fbe5
pkpraveen052 <40301280+pkpraveen052@users.noreply.github.com>
2026-03-25 13:43:49 +0530

    Uncomment import for account_move in __init__.py

--- custom-addons/metroerp_saas_customization/models/__init__.py
+++ custom-addons/metroerp_saas_customization/models/__init__.py
@@ -5,5 +5,5 @@ from . import res_config_settings
 from . import res_groups
 from . import res_partner
 from . import res_users
-#from . import account_move
+from . import account_move
 from . import sale_order_line
